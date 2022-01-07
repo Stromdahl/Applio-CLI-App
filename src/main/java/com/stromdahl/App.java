793ba5238da5cbba318c69cad001fc23246382dc
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2022-01-07 08:41:08 +0100

    cleanup

--- src/main/java/com/stromdahl/App.java
+++ src/main/java/com/stromdahl/App.java
@@ -9,25 +9,38 @@ import java.util.Properties;
 import com.stromdahl.applio.ApplioDataClient;
 import com.stromdahl.applio.UnauthorizedExeption;
 
+import okhttp3.Cache;
+
 public class App 
 {
+
+    public static String readAuthFromConfig() throws IOException{
+        InputStream input = new FileInputStream("src/resources/config.properties");
+        Properties properties = new Properties();           
+        properties.load(input);
+
+        return properties.getProperty("authKey");
+    }
+
+
+
     public static void main( String[] args ) throws IOException
     {
-        
-        try(InputStream input = new FileInputStream("src/resources/config.properties")){
-            Properties properties = new Properties();           
-            properties.load(input);
-
-            String authKey = properties.getProperty("authKey");
-            
-            ApplioDataClient applioDataClient = new ApplioDataClient(authKey);
-
-            System.out.println( applioDataClient.getDevices());
-
-        } catch (UnauthorizedExeption e){
-            System.out.println(e.getMessage());
-        } catch (IOException e){
-            e.printStackTrace();
+        String authKey = null;
+        try {
+            authKey = readAuthFromConfig();
+        } catch (IOException e) {
+            System.out.println("No config file was provided in the resources folder");
+            return;
         }
+            
+        if(authKey == null) {
+            System.out.println("No 'authKey' value was found in the config file ");
+            return;
+        }
+
+        ApplioDataClient applioDataClient = new ApplioDataClient(authKey);
+
+        System.out.println( applioDataClient.getDevices());
     }
 }
